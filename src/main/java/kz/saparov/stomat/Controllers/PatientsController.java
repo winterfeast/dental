0f8a95a3b8a2341519746741cce07e08c72cb3c5
SteveJobser <tima_tleg@mail.ru>
2021-04-19 01:13:11 +0600

    roll-back

--- src/main/java/kz/saparov/stomat/Controllers/PatientsController.java
+++ src/main/java/kz/saparov/stomat/Controllers/PatientsController.java
@@ -19,70 +19,68 @@ import kz.saparov.stomat.Services.PatientService;
 @Controller
 @RequestMapping("/patients")
 public class PatientsController {
-	private final PatientService patientService;
-	
-	@Autowired
+    private final PatientService patientService;
+
+    @Autowired
     public PatientsController(PatientService patientService) {
-	    	String autor = "Meiram123";
-        String autor2 = "Meiram123";
-		this.patientService = patientService;
-	}
+        this.patientService = patientService;
+    }
 
     @GetMapping()
     public String index(Model model) {
-    	model.addAttribute("patients", patientService.getLastTen());
+        model.addAttribute("patients", patientService.getLastTen());
         return "patients/index";
     }
-    
+
     @GetMapping("/{id}")
     public String show(@PathVariable("id") Long id, Model model) {
-    	model.addAttribute("dent",new Dent());
+        model.addAttribute("dent",new Dent());
         model.addAttribute("patient",patientService.get(id));
         return "patients/show";
     }
-    
+
     @GetMapping("/new")
     public String newPatient(@ModelAttribute("patient") Patient patient) {
         return "patients/new";
     }
-    
+
     @PostMapping()
     public String create(@ModelAttribute("patient") Patient patient) {
-    	patientService.save(patient);
+        patientService.save(patient);
         return "redirect:/patients";
     }
-    
+
     @GetMapping("/{id}/edit")
     public String edit(Model model, @PathVariable("id") Long id) {
         model.addAttribute("patient", patientService.get(id));
         return "patients/edit";
-    }    
-    
+    }
+
     @PatchMapping("/{id}")
     public String update(@ModelAttribute("patient") Patient patient,@PathVariable("id") Long id){
-    	patient.setId(id);
-    	patientService.save(patient);
-    	return "redirect:/patients";
-    }
-    
-    @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") Long id) {
-    	patientService.delete(id);
+        patient.setId(id);
+        patientService.save(patient);
         return "redirect:/patients";
     }
-    
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        patientService.delete(id);
+        return "redirect:/patients";
+    }
+
     @PostMapping("/{id}/dent")
     public String addDent(@ModelAttribute("dent") Dent dent,@PathVariable("id") Long id){
-    	Patient patient = patientService.get(id);
-    	dent.setPatient(patient);
-    	patient.addDent(dent);
-    	patientService.save(patient);
-    	return "redirect:/patients/{id}";
+        Patient patient = patientService.get(id);
+        dent.setPatient(patient);
+        patient.addDent(dent);
+        patientService.save(patient);
+        return "redirect:/patients/{id}";
     }
-    
+
     @GetMapping("/search")
     public String search(Model model, @RequestParam String keyword){
-    	model.addAttribute("patients", patientService.search(keyword.trim()));
-    	return"patients/search";
+        model.addAttribute("patients", patientService.search(keyword.trim()));
+        return"patients/search";
     }
 }
